d3c5d8f9d85b821bf68fd449e9ce9f005bb033dc
Nikita Sokolov <faucct@gmail.com>
2019-04-03 22:27:25 +0300

    HW07

--- Expression.hs
+++ Expression.hs
@@ -1,6 +1,12 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
 module Expression where
 
-import Text.Printf
+import           Combinators
+import           Control.Applicative
+import           Control.Monad
+import           Data.Char
+import           Text.Printf
 
 data Operator = Pow
               | Mul
@@ -15,14 +21,61 @@ data Operator = Pow
               | Gt
               | Conj
               | Disj
+              deriving Eq
 
 -- Simplest abstract syntax tree for expressions: only binops are allowed
 data EAst a = BinOp Operator (EAst a) (EAst a)
             | Primary a
+        deriving (Eq)
 
 -- Change the signature if necessary
-parseExpression :: String -> Either String (EAst Integer)
-parseExpression input = undefined
+parseExpression :: String -> Either [String] (EAst Integer)
+parseExpression input = do
+  (rest, eAST) <- runParser (spaced s) input
+  when (rest /= "") $ Left ["remaining input: " ++ rest]
+  return eAST
+ where
+  s  = b0 <|> n0
+  b0 = binOp Disj b1 b0 <|> b1
+  b1 = binOp Conj b2 b1 <|> b2
+  b2 =
+    binOp Eq n0 n0
+      <|> binOp Neq n0 n0
+      <|> binOp Le  n0 n0
+      <|> binOp Lt  n0 n0
+      <|> binOp Gt  n0 n0
+      <|> binOp Ge  n0 n0
+      <|> bracketed b0
+  n0 = leftAssociativeBinOps [Sum, Minus] n1
+  n1 = leftAssociativeBinOps [Mul, Div] n2
+  n2 = binOp Pow n3 n2 <|> n3
+  n3 =
+    bracketed n0
+      <|> Primary
+      .   read
+      <$> (   accept "0"
+          <|> (:)
+          <$> foldr ((<|>) . char) (failure []) "123456789"
+          <*> many (foldr ((<|>) . char) (failure []) "0123456789")
+          )
+  binOp operator left right =
+    BinOp operator <$> left <* spaced (accept (show operator)) <*> right
+  leftAssociativeBinOps operators value = foldr ($) <$> value <*> many
+    (foldr
+      ( (<|>)
+      . (\operator ->
+          (flip $ BinOp operator)
+            <$> (spaced (accept (show operator)) *> value)
+        )
+      )
+      (failure [])
+      operators
+    )
+  bracketed value = token '(' *> value <* token ')'
+  spaced value = many space *> value <* many space
+  space = Parser $ \case
+    symbol : rest | isSpace symbol -> Right (rest, symbol)
+    _                              -> Left []
 
 instance Show Operator where
   show Pow   = "^"
@@ -41,13 +94,17 @@ instance Show Operator where
 
 instance Show a => Show (EAst a) where
   show = show' 0
-    where
-      show' n t =
-        (if n > 0 then printf "%s|_%s" (concat (replicate (n - 1) "| ")) else id)
+   where
+    show' n t =
+      (if n > 0 then printf "%s|_%s" (concat (replicate (n - 1) "| ")) else id)
         (case t of
-                  BinOp op l r -> printf "%s\n%s\n%s" (show op) (show' (ident n) l) (show' (ident n) r)
-                  Primary x -> show x)
-      ident = (+1)
+          BinOp op l r -> printf "%s\n%s\n%s"
+                                 (show op)
+                                 (show' (ident n) l)
+                                 (show' (ident n) r)
+          Primary x -> show x
+        )
+    ident = (+ 1)
 
 {-
 show (BinOp Conj (BinOp Pow (Primary 1) (BinOp Sum (Primary 2) (Primary 3))) (Primary 4))
--- Main.hs
+++ Main.hs
@@ -10,13 +10,24 @@ import qualified Data.Set                      as Set
 
 main :: IO ()
 main = do
+  print $ Left ["remaining input: abc"] == parseExpression " 1 abc"
+  print
+    $   (BinOp
+          Conj
+          (BinOp Eq
+                 (BinOp Pow (Primary 1) (BinOp Sum (Primary 2) (Primary 3)))
+                 (Primary 1)
+          )
+          (BinOp Eq (Primary 4) (Primary 4)) ==
+        )
+    <$> parseExpression "  1 ^ (2 + 3) == 1 && 4 == 4  "
   fileNames <- getArgs
   mapM_
     (\fileName -> do
       input <- readFile fileName
       let a = parseExpression input
       putStrLn $ printf "Parsing %s\n" fileName
-      putStrLn $ either id show a
+      putStrLn $ either show show a
       putStrLn ""
     )
     fileNames
